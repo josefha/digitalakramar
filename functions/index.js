4d97342940f435bcb9cf219e90844d77db901471
Yuchen Che <yuchenc@kth.se>
2020-05-26 11:13:00 +0800

    Work in progress

--- functions/index.js
+++ functions/index.js
@@ -66,3 +66,47 @@ exports.sendSms = functions.https.onRequest((request, response) => {
         response.send("Error: " + reason)
     })
 })
+
+exports.swishPayment = functions.https.onRequest((request, response) => {
+    console.log("in swish payment function")
+    const number = request.query.number
+    // const amount = request.query.amount
+    const amount = 1
+
+    const testConfig = {	
+        payeeAlias: "1231181189",
+        host: "https://mss.cpc.getswish.net/swish-cpcapi",
+        cert: path.resolve(__dirname, 'ssl/test.pem'),
+        key: path.resolve(__dirname, 'ssl/test.key'),
+        ca: path.resolve(__dirname, 'ssl/test_ca.pem'),
+    }
+    
+    const config = testConfig
+
+    const postFields = {
+        payeePaymentReference: "0123456789",
+		callbackUrl: "https://webhook.site/a8f9b5c2-f2da-4bb8-8181-fcb84a6659ea",
+		payeeAlias: config.payeeAlias,
+		payerAlias: number,
+		amount: amount,
+		currency: "SEK"
+    }
+
+    const postData = querystring.stringify(postFields)
+
+    rp({
+        uri:  config.host + '/api/v1/paymentrequests',
+        method: 'POST',
+        body: postData
+    }).then(result => {
+        const resultJson = JSON.parse(result)
+        if(resultJson.status.localeCompare("failed") == 0){
+            response.send("Failed.")
+        } else {
+            response.send("Succeed")
+        }
+        console.log(result)
+    }).catch(reason => {
+        response.send("Error: " + reason)
+    })
+})
