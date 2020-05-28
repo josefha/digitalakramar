a1d7db53e1550bf95f2d216fa6e80609cf99141d
robertcarlsson <robert.carlsson1987@gmail.com>
2020-05-28 21:00:47 +0200

    Changed structure of cloud funcionts

--- functions/api/firestore.js
+++ functions/api/firestore.js
@@ -6,37 +6,6 @@ const crypto = require("crypto");
 const shortid = require("shortid");
 const rp = require("request-promise");
 const querystring = require("querystring");
-// path: `.env.${process.env.NODE_ENV}`,
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-/*
-exports.checkRecaptcha = functions.https.onRequest((req, res) => {
-  const response = req.query.response;
-  console.log("recaptcha response", response);
-  rp({
-    uri: "https://recaptcha.google.com/recaptcha/api/siteverify",
-    method: "POST",
-    formData: {
-      secret: functions.config().recaptcha.secret,
-      response: response,
-    },
-    json: true,
-  })
-    .then((result) => {
-      console.log("recaptcha result", result);
-      if (result.success) {
-        res.status(200).send("You're good to go, human.");
-      } else {
-        res.status(401).send("Recaptcha verification failed. Are you a robot?");
-      }
-    })
-    .catch((reason) => {
-      console.log("Recaptcha request failure", reason);
-      res.status(500).send("Recaptcha request failed.");
-    });
-});
 
 exports.dataBaseTest = functions.https.onRequest((request, response) => {
   db = admin.firestore();
@@ -52,7 +21,7 @@ exports.dataBaseTest = functions.https.onRequest((request, response) => {
     response.send(`Retrieved data: ${JSON.stringify(data)}`);
   });
 });
-*/
+
 exports.getHugger = functions.https.onRequest((request, response) => {
   db = admin.firestore();
   let huggerName = "";
@@ -158,12 +127,10 @@ exports.createReciever = functions.https.onRequest((request, response) => {
       });
   }
 });
-/*
-exports.stopSms = functions.https.onRequest((request, response) => {
-  /*
-  This function will stop sms towards a specific phone number, 
-  it is activated from the link a
 
+exports.stopSms = functions.https.onRequest((request, response) => {
+  //This function will stop sms towards a specific phone number,
+  //it is activated from the link a
 
   db = admin.firestore();
 
@@ -177,10 +144,7 @@ exports.stopSms = functions.https.onRequest((request, response) => {
 
     response.send("Checking back" + documentSnapshot);
   });
-
-  
 });
-*/
 
 exports.sendSms = functions.https.onRequest((request, response) => {
   const createNewReciever = (numberHash) => {
@@ -249,35 +213,3 @@ exports.sendSms = functions.https.onRequest((request, response) => {
       console.log("Error recieving" + error);
     });
 });
-
-/*
-  const postFields = {
-    from: "DigitalKram",
-    to: request.body.recipients[0],
-    message: "DIGITAL HUGS SAVE THE WORLD",
-  };
-
-  // Instead of stringify here, json: true in the rp call shold allow stringification
-  const postData = querystring.stringify(postFields);
-
-  rp({
-    uri: "https://" + username + ":" + password + "@api.46elks.com/a1/SMS",
-    method: "POST",
-    body: postData,
-  })
-    .then((result) => {
-      const resultJson = JSON.parse(result);
-      if (resultJson.status.localeCompare("failed") == 0) {
-        response.send("Failed.");
-      } else {
-        response.send("SMS is sent!");
-      }
-      console.log(result);
-    })
-    .catch((reason) => {
-      response.send("Error: " + reason);
-    });
-    */
-/*
-});
-*/
