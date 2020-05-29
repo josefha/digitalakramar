ae27f358d67243845a80e3b5ad0d77cef945f5b6
robertcarlsson <robert.carlsson1987@gmail.com>
2020-05-29 10:26:27 +0200

    Changed back elk to work with parameters

--- functions/api/elk.js
+++ functions/api/elk.js
@@ -4,49 +4,6 @@ const querystring = require("querystring");
 
 exports.sendSms = functions.https.onRequest((request, response) => {
   const number = request.query.number;
-
-  const createNewReciever = (numberHash) => {
-    db.collection("recievers");
-    console.log("Got into the create function");
-  };
-  const validatedRequestBody = (body) => {
-    return body && body.recievers && body.senderName;
-  };
-
-  if (!validatedRequestBody(request.body)) {
-    response.send("Cloud function sendSms getting wrong data");
-    return;
-  }
-
-  // Setup for firestore and elks api service
-  db = admin.firestore();
-  const username = functions.config().elks.username;
-  const password = functions.config().elks.password;
-
-  /*
-    "senderName": "Robert",
-    "recievers": ["+46700383373"]
-  */
-  //const statusRecievers = [{ index: -1, reason: "schema" }];
-
-  request.body.recievers.forEach((number, i) => {
-    console.log("Entered the array");
-    const numberHash = crypto
-      .createHash("sha256")
-      .update(number)
-      .digest("base64");
-
-    //  const recieverReference =
-    db.collection("recievers")
-      .where("mobileNumberHash", "==", "34GH35")
-      .get()
-      .then((allSnapshots) => {
-        allSnapshots.forEach((snapshot) => {
-          response.send(`End of function: ${JSON.stringify(snapshot.data())}`);
-        });
-      });
-  });
-
   const username = functions.config().elks.username;
   const password = functions.config().elks.password;
 
